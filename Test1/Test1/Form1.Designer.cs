9da12a8d46aa035961cec1f072d749695a2a94eb
deniska <xumuk39rus@yandex.ru>
2019-06-23 14:57:58 +0300

    All
    Test project

---
+++ Test1/Test1/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Test1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.comboBox4 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(45, 23);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Файл";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.SplitButton;
+			this.textBox1.AllowDrop = true;
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Location = new System.Drawing.Point(45, 153);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(443, 213);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.WordWrap = false;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(45, 67);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "Применить";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(205, 24);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(205, 71);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(388, 71);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 7;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(388, 24);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 6;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+			this.comboBox1.Location = new System.Drawing.Point(149, 23);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(50, 21);
+			this.comboBox1.TabIndex = 8;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+			this.comboBox2.Location = new System.Drawing.Point(149, 71);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(50, 21);
+			this.comboBox2.TabIndex = 9;
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+			this.comboBox3.Location = new System.Drawing.Point(332, 71);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(50, 21);
+			this.comboBox3.TabIndex = 10;
+			// 
+			// comboBox4
+			// 
+			this.comboBox4.FormattingEnabled = true;
+			this.comboBox4.Items.AddRange(new object[] {
+            "И",
+            "ИЛИ"});
+			this.comboBox4.Location = new System.Drawing.Point(332, 24);
+			this.comboBox4.Name = "comboBox4";
+			this.comboBox4.Size = new System.Drawing.Size(50, 21);
+			this.comboBox4.TabIndex = 11;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(531, 391);
+			this.Controls.Add(this.comboBox4);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.MinimumSize = new System.Drawing.Size(547, 430);
+			this.Name = "Form1";
+			this.Text = "Hello";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.ComboBox comboBox4;
+	}
+}
+
---
+++ Test1/Test1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Test1
+{
+	public partial class Form1 : Form
+	{
+		private string		glob_text;
+		private bool		is_selected;
+		private ComboBox[]	comboBoxes;
+		private TextBox[]	textBoxes;
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			openFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+			saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+			foreach (TextBox textB in this.Controls.OfType<TextBox>())
+			{
+				textB.Click += new EventHandler(textB_Click);
+			}
+			this.is_selected = false;
+			this.glob_text = null;
+			this.textBoxes = new TextBox[4];
+			textBoxes[0] = textBox2;
+			textBoxes[1] = textBox3;
+			textBoxes[2] = textBox4;
+			textBoxes[3] = textBox5;
+			this.comboBoxes = new ComboBox[4];
+			comboBoxes[0] = comboBox1;
+			comboBoxes[1] = comboBox2;
+			comboBoxes[2] = comboBox3;
+			comboBoxes[3] = comboBox4;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+				return;
+			string filename = openFileDialog1.FileName;
+			this.glob_text = System.IO.File.ReadAllText(filename, Encoding.GetEncoding(1251));
+			textBox1.Text = this.glob_text;
+			this.is_selected = false;
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+			if (this.is_selected == false)
+				this.glob_text = this.textBox1.Text;
+		}
+
+		private void textB_Click(object sender, EventArgs e)
+		{
+			this.textBox1.Text = this.glob_text;
+			this.is_selected = false;
+		}
+
+		private String find_string(String[] raw_str, ComboBox[] comboBoxes, TextBox[] textBoxes)
+		{
+			String	res;
+			bool	is_find;
+			int		k;
+			Boolean or_flag;
+			Boolean and_falg;
+			int		i;
+
+			res = null;
+			is_find = false;
+			k = 0;
+			while (k < raw_str.Length)
+			{
+				and_falg = true;
+				or_flag = false;
+				i = 0;
+				while (i < 4)
+				{
+					if ((comboBoxes[i].SelectedIndex != -1) && (textBoxes[i].TextLength > 0))
+					{
+						if (comboBoxes[i].SelectedIndex == 0)
+							and_falg = and_falg && raw_str[k].Contains(textBoxes[i].Text);
+						else
+							or_flag = or_flag || raw_str[k].Contains(textBoxes[i].Text);
+					}
+					i++;
+				}
+				is_find = and_falg || or_flag;
+				if (is_find)
+					res = res + raw_str[k] + "\r\n";
+				k++;
+			}
+			return (res);
+		}
+
+		private Boolean check_sets(ComboBox[] comboBoxes, TextBox[] textBoxes)
+		{
+			int		i;
+			Boolean	res;
+
+			res = false;
+			i = 0;
+			while (i < 4)
+			{
+				if ((comboBoxes[i].SelectedIndex != -1) && (textBoxes[i].TextLength > 0))
+					res = res || true;
+				i++;
+			}
+			return (res);
+		}
+
+		void	whole_blovk()
+		{
+			int i;
+
+			this.button1.Enabled = false;
+			this.button2.Enabled = false;
+			this.textBox1.Enabled = false;
+			i = 0;
+			while (i < 4)
+			{
+				this.textBoxes[i].Enabled = false;
+				this.comboBoxes[i].Enabled = false;
+				i++;
+			}
+		}
+
+		void whole_unblovk()
+		{
+			int i;
+
+			this.button1.Enabled = true;
+			this.button2.Enabled = true;
+			this.textBox1.Enabled = true;
+			i = 0;
+			while (i < 4)
+			{
+				this.textBoxes[i].Enabled = true;
+				this.comboBoxes[i].Enabled = true;
+				i++;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int			i;
+			String[]	some_str;
+
+			this.is_selected = true;
+			whole_blovk();
+			if (this.glob_text != null)
+			{
+
+				if (check_sets(this.comboBoxes, this.textBoxes))
+				{
+					some_str = this.glob_text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+					this.textBox1.Text = find_string(some_str, this.comboBoxes, this.textBoxes);
+				}
+				else
+					MessageBox.Show("Настройте хотя бы один из фильтров!");
+			}
+			else
+				MessageBox.Show("Выберите файл!");
+			whole_unblovk();
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.textBox1.AllowDrop = true;
+			this.textBox1.DragEnter += new DragEventHandler(textBox1_DragEnter);
+			textBox1.DragDrop += new DragEventHandler(textBox1_DragDrop);
+		}
+
+		private void textBox1_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+				e.Effect = DragDropEffects.All;
+			else
+				e.Effect = DragDropEffects.None;
+		}
+
+		private void textBox1_DragDrop(object sender, DragEventArgs e)
+		{
+			string[]	strings;
+			string		filename;
+			string[]	parse;
+
+			strings = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+			filename = strings[0];
+			parse = filename.Split('.');
+			if (parse[parse.Length - 1] == "txt")
+			{
+				this.glob_text = System.IO.File.ReadAllText(filename, Encoding.GetEncoding(1251));
+				textBox1.Text = this.glob_text;
+				this.is_selected = false;
+			}
+			else
+				MessageBox.Show("Неверный тип файла!");
+		}
+	}
+}
